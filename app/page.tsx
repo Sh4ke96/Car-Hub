b3a6981b2f45364541d5a17c331d5952eb046a25
Artur Michałek <sh4ke96@gmail.com>
2023-07-10 11:24:11 +0200

    add new props to button, add car card, car details

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { fetchCars } from "@/utils";
 
 export default async function Home() {
   const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -18,6 +19,20 @@ export default async function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-xl font-bold text-black">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,15 @@
 "use client";
 import { ButtonProps } from "@/types";
+import Image from "next/image";
 
-const Button = ({ title, containerStyles, handleClick, type }: ButtonProps) => {
+const Button = ({
+  title,
+  containerStyles,
+  handleClick,
+  type,
+  textStyles,
+  rightIcon,
+}: ButtonProps) => {
   return (
     <button
       disabled={false}
@@ -9,7 +17,17 @@ const Button = ({ title, containerStyles, handleClick, type }: ButtonProps) => {
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right arrow icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
